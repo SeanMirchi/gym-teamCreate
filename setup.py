a837fb55f0844255dae7152d71b82d9a134c7d3d
Sean Mirchi <sean.mirchi@grandcentrix.net>
2019-03-20 14:32:48 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup
 
 setup(name='player_selector',
       version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
+      install_requires=['gym>=0.2.3']
 )
